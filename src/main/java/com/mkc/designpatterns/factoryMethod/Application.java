266c5139a30e56eb6813356ce69b5d9f1233c75b
Marlon Klagenberg <marlon.klc@gmail.com>
2017-07-11 21:33:32 -0300

    Ajuste de gitignore ; Ajuste na factoryMethod

--- src/main/java/com/mkc/designpatterns/factoryMethod/Application.java
+++ src/main/java/com/mkc/designpatterns/factoryMethod/Application.java
@@ -3,35 +3,22 @@ package com.mkc.designpatterns.factoryMethod;
 import java.util.Scanner;
 import com.mkc.designpatterns.factoryMethod.console.LoggerConsole;
 import com.mkc.designpatterns.factoryMethod.logger.Logger;
-import com.mkc.designpatterns.factoryMethod.txt.LoggerTxt;
 
 public class Application {
 
 	public static void main(String[] args) {
 
-		new Application().testaConsole();
-		//new Application().testaTxt();
+		Logger loggerConsole = new LoggerConsole();
+		//Logger loggerTxt = new LoggerTxt();
+
+		new Application().log(loggerConsole);
 	}
 
-	public void testaConsole() {
-		Logger logger = new LoggerConsole();
-
+	public void log(Logger logger) {
 		Scanner scanner = new Scanner(System.in);
 		if (scanner.hasNext()) {
 			String mensagem = scanner.nextLine();
 			logger.log(mensagem);
 		}
 	}
-
-	public void testaTxt() {
-		Logger logger = new LoggerTxt();
-
-		Scanner scanner = new Scanner(System.in);
-		if (scanner.hasNext()) {
-			String mensagem = scanner.next();
-			logger.log(mensagem);
-		}
-	}
-
-
 }
